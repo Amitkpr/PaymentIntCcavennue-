386ce53ed2708ed98ba8fb225d89d71520a8cacc
Amit kapoor <kapoor.amit15@gmail.com>
2018-12-06 16:28:32 +0530

    Add files via upload

---
+++ ccavRequestHandler.php
@@ -0,0 +1,34 @@
+<html>
+<head>
+<title> Non-Seamless-kit</title>
+</head>
+<body>
+<center>
+
+<?php include('Crypto.php')?>
+<?php 
+
+	error_reporting(0);
+	
+	$merchant_data='165304';
+	$working_key='60AD1E08AF7DC9012C38A0428D4B50E8';//Shared by CCAVENUES
+	$access_code='AVWS76FB01AJ53SWJA';//Shared by CCAVENUES
+	
+	foreach ($_POST as $key => $value){
+		$merchant_data.=$key.'='.$value.'&';
+	}
+
+	$encrypted_data=encrypt($merchant_data,$working_key); // Method for encrypting the data.
+
+?>
+<form method="post" name="redirect" action="https://secure.ccavenue.com/transaction/transaction.do?command=initiateTransaction"> 
+<?php
+echo "<input type=hidden name=encRequest value=$encrypted_data>";
+echo "<input type=hidden name=access_code value=$access_code>";
+?>
+</form>
+</center>
+<script language='javascript'>document.redirect.submit();</script>
+</body>
+</html>
+
